eabcd44f2ed7b664532bad052ce4246c408ad614
brucemcl <bruce@opencollab.co.za>
2024-10-21 11:44:42 +0200

    UWCI-177 message output

--- impl/src/main/java/coza/opencollab/sakai/cm/uwc/SASIIntegrationClient.java
+++ impl/src/main/java/coza/opencollab/sakai/cm/uwc/SASIIntegrationClient.java
@@ -329,7 +329,6 @@ public class SASIIntegrationClient implements SISClient {
 		Download_CalendarGroup_Output calendarOutput = null;
 		SISAcademicSession academicSession = null;
 		try {
-			log.error("###Token 1: " + _token);
 			calendarOutput = proxy.getSakaiSoap().download_CalendarGroup(
 					academicInput, _token);
 			String[][] academicCalendarArray = calendarOutput
